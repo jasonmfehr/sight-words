ecb8d79fd8c53b21fd863a72fb492321041fe1e6
Jason Fehr <jasonmfehr@gmail.com>
2016-03-30 22:00:22 -0500

    added database access methods

--- db/db.js
+++ db/db.js
@@ -1,30 +1,95 @@
-var pg = require('pg');
+const pg = require('pg');
+const uuid = require('uuid')
 
-pg.defaults.ssl = true;
-/*
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+//pg.defaults.ssl = true;
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-*/
-
-exports.listUsers = (callback) => {
-  //pg.connect('postgres://postgres:postgres@localhost:5432/jasonmfehr', function(err, client, done) {
+exports.hasInProgressGame = (personId, callback) => {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     if (err) throw err;
 
-    client.query('SELECT "id","user_name","grade","min_level","max_level" from users;', function(err, results){
-      if (err) throw err;
-      
-      done();
-      
-      callback(results.rows);
-    });
+    client.query(
+      {
+        name: 'check for in progress',
+        text: 'SELECT id FROM people_data WHERE person_id=$1 AND end_time IS NULL;',
+        values: [personId]
+      }, 
+      function(err, results){
+        if (err) throw err;
+
+        done();
+
+        callback(results.rows.length > 0);
+      }
+    );
   });
-}
+};
+
+exports.addGame = (personId, jsonData, callback) => {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) throw err;
+
+    const newUuid = uuid.v4();
+    
+    client.query(
+      {
+        name: 'add game',
+        text: 'INSERT INTO people_data (id,person_id,person_data) VALUES ($1,$2,$3);',
+        values: [newUuid, personId, jsonData]
+      }, 
+      function(err, results){
+        if (err) throw err;
+
+        done();
+
+        callback(newUuid);
+      }
+    );
+  });
+};
+
+exports.saveGame = (gameId, jsonData, callback) => {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) throw err;
+
+    client.query(
+      {
+        name: 'save game',
+        text: 'UPDATE people_data SET person_data=$1 WHERE id=$2;',
+        values: [jsonData, gameId]
+      }, 
+      function(err, results){
+        if (err) throw err;
+
+        done();
+        
+        if(callback){
+          callback(results.rowCount);
+        }
+      }
+    );
+  });
+};
+
+exports.endGame = (gameId, callback) => {
+   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) throw err;
+
+    client.query(
+      {
+        name: 'end game',
+        text: 'UPDATE people_data SET end_time=now() WHERE id=$1;',
+        values: [gameId]
+      }, 
+      function(err, results){
+        if (err) throw err;
+
+        done();
+        
+        if(callback){
+          callback(results.rowCount);
+        }
+      }
+    );
+  });
+};
+
