6d1d3cb1a476d78126c017a640d567dc847970ac
Jason Fehr <jasonmfehr@gmail.com>
2016-03-30 20:57:52 -0500

    updated db structure

--- db/sql/version1.sql
+++ db/sql/version1.sql
@@ -2,9 +2,6 @@ CREATE TABLE public.users
 (
    id uuid NOT NULL, 
    user_name character varying(250) NOT NULL, 
-   grade character varying(5), 
-   min_level smallint NOT NULL DEFAULT 0, 
-   max_level smallint NOT NULL DEFAULT 32767, 
    CONSTRAINT users_pk PRIMARY KEY (id)
 ) 
 WITH (
@@ -12,15 +9,31 @@ WITH (
 )
 ;
 
-CREATE TABLE public.user_data
+CREATE TABLE public.people
 (
    id uuid NOT NULL, 
    user_id uuid NOT NULL, 
-   user_data jsonb NOT NULL, 
+   person_name character varying(250) NOT NULL, 
+   grade character varying(5), 
+   min_level smallint NOT NULL DEFAULT 0, 
+   max_level smallint NOT NULL DEFAULT 32767, 
+   CONSTRAINT user_people_fk FOREIGN KEY (user_id) REFERENCES public.users (id) ON UPDATE CASCADE ON DELETE CASCADE, 
+   CONSTRAINT people_pk PRIMARY KEY (id)
+) 
+WITH (
+  OIDS = FALSE
+)
+;
+
+CREATE TABLE public.people_data
+(
+   id uuid NOT NULL, 
+   person_id uuid NOT NULL, 
+   person_data jsonb NOT NULL, 
    start_time timestamp with time zone, 
    end_time timestamp with time zone, 
-   CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES public.users (id) ON UPDATE NO ACTION ON DELETE NO ACTION, 
-   CONSTRAINT user_data_pk PRIMARY KEY (id)
+   CONSTRAINT people_people_data_fk FOREIGN KEY (person_id) REFERENCES public.people (id) ON UPDATE CASCADE ON DELETE CASCADE, 
+   CONSTRAINT people_data_pk PRIMARY KEY (id)
 ) 
 WITH (
   OIDS = FALSE
